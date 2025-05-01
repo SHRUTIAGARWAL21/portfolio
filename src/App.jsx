115e7937309920a6dac9760435c51e774d0d8be7
shrutiagarwal21 <khushiagarwal0204@gmail.com>
2025-05-02 02:50:31 +0530

    overflow fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,31 @@ import Footer from "./Footer";
 
 export default function App() {
   return (
-    <div className="md:mx-0 xl:mx-28 border-l border-r">
-      <Navbar />
+    <div className="relative min-h-screen overflow-x-hidden">
+      <div className="md:mx-0 xl:mx-28 border-l border-r">
+        <Navbar />
 
-      <div className="relative flex">
-        <div
-          className="hidden md:block w-8 
+        <div className="relative flex">
+          <div
+            className="hidden md:block w-8 
                bg-[repeating-linear-gradient(135deg,#e5e7eb_0px,#e5e7eb_1px,transparent_1px,transparent_3px)]"
-        />
+          />
 
-        <div className="flex-1 border-l border-r border-gray-200">
-          <Intro />
-          <About />
-          <Project />
-          <Contact />
+          <div className="flex-1 border-l border-r border-gray-200">
+            <Intro />
+            <About />
+            <Project />
+            <Contact />
+          </div>
+
+          <div
+            className="hidden md:block w-8 
+               bg-[repeating-linear-gradient(135deg,#e5e7eb_0px,#e5e7eb_1px,transparent_1px,transparent_3px)]"
+          />
         </div>
 
-        <div
-          className="hidden md:block w-8 
-               bg-[repeating-linear-gradient(135deg,#e5e7eb_0px,#e5e7eb_1px,transparent_1px,transparent_3px)]"
-        />
+        <Footer />
       </div>
-
-      <Footer />
     </div>
   );
 }
